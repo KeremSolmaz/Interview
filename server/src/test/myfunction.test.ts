b6d7a480b76de43eca73687ff8b9cf8b3fb44ae7
KeremSolmaz <kerem@dak2go.com>
2022-03-29 22:16:04 -0400

    First Test Passed(With Mocha)

--- server/src/test/myfunction.test.ts
+++ server/src/test/myfunction.test.ts
@@ -1,12 +1,13 @@
 import {countnumber,getMedian} from '../myfunction';
 import { expect } from 'chai';
 
-describe('Primary Number finder', () => {
-    it('should be worked', () => {
+
+describe('Primary Number Finder', () => {
+    it('Prime Number Funtion Test', () => {
       const result = countnumber(10);
       console.log(result);
-      var kerem[];
-      // = "[ 2, 3, 5, 7 ]"
-      expect(result).to.equal('2, 3, 5, 7');
+      const arr1 = [ 2, 3, 5, 7 ];
+   
+      expect(result).to.eql(arr1);
     });
   });
