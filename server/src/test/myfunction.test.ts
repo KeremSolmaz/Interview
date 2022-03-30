c680e977a6016cd3fab40e54deec7d5f14812323
KeremSolmaz <kerem@dak2go.com>
2022-03-29 22:34:18 -0400

    Server Side All Tests Passed(With Mocha) Test: Prime Number, Median even, Median odd

--- server/src/test/myfunction.test.ts
+++ server/src/test/myfunction.test.ts
@@ -11,3 +11,22 @@ describe('Primary Number Finder', () => {
       expect(result).to.eql(arr1);
     });
   });
+
+  describe('Median Finder For Even Count', () => {
+    it('Median Even Test', () => {
+      const arr1 = [ 2, 3, 5, 7 ];
+      const result = getMedian(arr1);
+      const arr2 = [ 3, 5 ];
+      expect(result).to.eql(arr2);
+    });
+  });
+
+  describe('Median Finder For odd Count', () => {
+    it('Median odd Test', () => {
+      const arr1 = [ 2, 3, 5 ];
+      const result = getMedian(arr1);
+      console.log(result);
+      const arr2 = [3];
+      expect(result).to.eql(arr2);
+    });
+  });
